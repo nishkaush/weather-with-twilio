9599645629629fbdeca8ba111e0eae04b021e483
nishkaush <kaushish.nishant@hotmail.com>
2018-03-15 01:27:07 +1100

    still fixing heroku

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post("/", (req, res) => {
     .send(twiml.toString());
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("app running on port 3000");
 });
